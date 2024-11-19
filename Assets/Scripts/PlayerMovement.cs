9b0a5f438090e42b907e5717fa183ba7d2ffb842
Mike L <mluning4669@gmail.com>
2024-11-18 20:59:34 -0600

    Add movement script along with moving and jumping - ML

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 1.0f;
+    [SerializeField] private Rigidbody2D rb;
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
+        
+        //flip player image when moving left and right
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, speed);
+        }
+    }
+}
