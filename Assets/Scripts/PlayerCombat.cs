c5b59c7a83c5c09f96cd7abdaef9e60e2fad710c
Mike L <mluning4669@gmail.com>
2024-11-20 13:39:54 -0600

    Flesh out player combat a little - ML

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -6,14 +6,28 @@ using UnityEngine;
 public class PlayerCombat : MonoBehaviour
 {
     [SerializeField] private Animator anim;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float weaponRange;
+    [SerializeField] private LayerMask enemyLayer;
 
     public void Attack()
     {
         anim.SetBool("IsAttacking", true);
     }
 
+    public void DealDamage()
+    {
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, enemyLayer); 
+    }
+
     public void FinishAttacking()
     {
         anim.SetBool("IsAttacking", false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, 0.5f);
     }
 }
