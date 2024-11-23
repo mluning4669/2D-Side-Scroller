dc5600337de7961af7b65d362a40416d89d32120
Mike L <mluning4669@gmail.com>
2024-11-22 23:37:48 -0600

    Add logic to chase player sans jumping - ml

--- Assets/Scripts/EnemyScripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -1,18 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
-public class EnemyMovement : MonoBehaviour
+public enum EnemyState
 {
+    Idle,
+    Chasing,
+}
+public class EnemyMovement : MonoBehaviour
+{   
+
+    [SerializeField] private float speed;
+    [SerializeField] private Transform detectionPoint;
+    [SerializeField] private float playerDetectRange;
+    [SerializeField] private LayerMask playerLayer;
+
+    private EnemyState enemyState;
+    private int facingDirection = -1;
+    private Transform player;
+    private Rigidbody2D rb;
+    private Animator anim;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        ChangeState(EnemyState.Idle);
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        CheckForPlayer();
+    }
+
+    void Chase()
+    {
+        if (player.position.x > transform.position.x && facingDirection == -1 ||
+                 player.position.x < transform.position.x && facingDirection == 1)
+        {
+            Flip();
+        }
+
+        rb.velocity = new Vector2(facingDirection * speed, rb.velocity.y);
+    }
+
+    private void CheckForPlayer()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(detectionPoint.position, playerDetectRange, playerLayer);
+
+        if (hits.Length > 0)
+        {
+            player = hits[0].transform;
+            Chase();
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+            ChangeState(EnemyState.Idle);
+        }
+
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    public void ChangeState(EnemyState newState)
+    {
+        switch (enemyState)
+        {
+            case EnemyState.Idle:
+                anim.SetBool("IsIdle", false);
+                break;
+
+            case EnemyState.Chasing:
+                anim.SetBool("IsChasing", false);
+                break;
+        }
+
+        enemyState = newState;
+
+        switch (enemyState)
+        {
+            case EnemyState.Idle:
+                anim.SetBool("IsIdle", true);
+                break;
+
+            case EnemyState.Chasing:
+                anim.SetBool("IsChasing", true);
+                break;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(detectionPoint.position, playerDetectRange);
     }
 }
