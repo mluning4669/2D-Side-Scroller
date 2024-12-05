71b72fe45e6cc88a7c25e1061cf36f7e8f245923
Mike L <mluning4669@gmail.com>
2024-12-05 17:02:43 -0600

    Give enemies health

---
+++ Assets/Scripts/EnemyScripts/EnemyHealth.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth;
+
+    public void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public bool ChangeHealth(int amount)
+    {
+        currentHealth += amount;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        else if (currentHealth <= 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
